461619915e590b55af29571d9161a1f8d965ae4d
oujun <me@oujun.work>
2023-09-15 11:48:38 +0800

    增加头信息

--- Bilibili.Live.Checker/Program.cs
+++ Bilibili.Live.Checker/Program.cs
@@ -1,8 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Net.Http.Headers;
-using System.Text.Json;
-using System.Runtime.Caching;
 using Microsoft.Extensions.Configuration;
+using System.Runtime.Caching;
+using System.Text.Json;
 
 ConfigurationBuilder builder = new();
 builder.AddJsonFile("appsetting.json", true, true);
@@ -27,8 +26,20 @@ foreach (var user in configuration.Users)
 
 using HttpClient client = new();
 client.DefaultRequestHeaders.Accept.Clear();
-client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
 client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.76");
+//client.DefaultRequestHeaders.Add(":authority:", "api.bilibili.com");
+//client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br");
+client.DefaultRequestHeaders.Add("Accept-Language", "zh-CN,zh;q=0.9");
+client.DefaultRequestHeaders.Add("Cache-Control", "no-cache");
+client.DefaultRequestHeaders.Add("Origin", "https://space.bilibili.com");
+client.DefaultRequestHeaders.Add("Dnt", "1");
+client.DefaultRequestHeaders.Add("Pragma", "no-cache");
+client.DefaultRequestHeaders.Add("Sec-Ch-Ua", "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Microsoft Edge\";v=\"116\"");
+client.DefaultRequestHeaders.Add("Sec-Ch-Ua-Mobile", "?0");
+client.DefaultRequestHeaders.Add("Sec-Ch-Ua-Platform", "\"Windows\"");
+client.DefaultRequestHeaders.Add("Sec-Fetch-Dest", "empty");
+client.DefaultRequestHeaders.Add("Sec-Fetch-Mode", "cors");
+client.DefaultRequestHeaders.Add("Sec-Fetch-Site", "same-site");
 var wechatPush = new PushWeChatMessage(configuration?.APPTOKEN ?? "");
 Console.WriteLine("后台轮询定时器准备");
 using var timer = new PeriodicTimer(TimeSpan.FromSeconds(interval));
@@ -45,8 +56,11 @@ while (await timer.WaitForNextTickAsync())
             var item = (FollowInfo?)cache.Get(uid);
             if (item == null) continue;
             cache.Remove(uid);
+            client.DefaultRequestHeaders.Remove("Referer");
+            client.DefaultRequestHeaders.Add("Referer", $"https://space.bilibili.com/{item.UID}/");
             var json = await client.GetStringAsync($"https://api.bilibili.com/x/space/wbi/acc/info?mid={item.UID}&token=&platform=web");
             var bilibiliScapeInfo = JsonSerializer.Deserialize<BilibiliResponse<BilibiliSpaceInfo>>(json);
+            if (bilibiliScapeInfo.Code != 0) Console.WriteLine(json);
             item.Status = (bilibiliScapeInfo?.Data?.LiveRoom?.LiveStatus ?? 0) == 1;
             if (item.IsNotify)
             {
--- Bilibili.Live.Checker/PushWeChatMessage.cs
+++ Bilibili.Live.Checker/PushWeChatMessage.cs
@@ -24,6 +24,7 @@ public class PushWeChatMessage
     public async Task SendAsync(string[] uids, string[] topicIds, string summary, string content, string url = "")
     {
         using HttpClient send = new();
+        Console.WriteLine($"推送信息给UID{string.Join('、', uids)}，TopicId{string.Join(',', topicIds)}");
         var sendRequest = new HttpRequestMessage
         {
             Method = HttpMethod.Post,
@@ -32,7 +33,7 @@ public class PushWeChatMessage
             {
                 appToken = AppToken,
                 content,
-                //summary,//消息摘要，显示在微信聊天页面或者模版消息卡片上，限制长度100，可以不传，不传默认截取content前面的内容。
+                summary,//消息摘要，显示在微信聊天页面或者模版消息卡片上，限制长度100，可以不传，不传默认截取content前面的内容。
                 contentType = 3,//内容类型 1表示文字  2表示html(只发送body标签内部的数据即可，不包括body标签) 3表示markdown 
                 uids,
                 topicIds,
